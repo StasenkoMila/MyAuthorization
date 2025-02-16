2ac9dc0d50227e92792786dd30bc4ea5c1492276
StasenkoMila <milanastasenko5@gmail.com>
2025-02-16 20:38:47 +0300

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,13 +13,7 @@ import servlets.SignUpServlet;
 
 import javax.servlet.Servlet;
 
-/**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
+
 public class Main {
     public static void main(String[] args) throws Exception {
         AccountService accountService = new AccountService();
